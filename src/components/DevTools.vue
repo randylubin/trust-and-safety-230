42f20614d3a12de274c8dc74ca1eebe29c082e3c
Randy Lubin <randylubin@gmail.com>
2023-04-05 16:11:50 -0700

    add queue data to devtools panel

--- src/components/DevTools.vue
+++ src/components/DevTools.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { GameSessionStore } from '../stores/GameSessionStore'
+import { IssueQueueStore } from '../stores/IssueQueueStore'
 </script>
 
 <template>
@@ -7,7 +8,11 @@ import { GameSessionStore } from '../stores/GameSessionStore'
     <h1>Dev Tools</h1>
     <button @click="GameSessionStore.timesUp(true)">Set Timer to Zero</button>
     <div>Current Round: {{ GameSessionStore.currentRound }}</div>
-    <div>Time remaining: {{ GameSessionStore.timeRemaining }}</div>
+    <div>Time Remaining: {{ GameSessionStore.timeRemaining }}</div>
+    <div>Current Queue: {{ IssueQueueStore.currentIssueQueue.length }}</div>
+    <div>
+      Unprocessed Queue: {{ IssueQueueStore.unprocessedFollowUps.length }}
+    </div>
     <div>Manager Disagreements {{ GameSessionStore.disagreeWithManager }}</div>
     <div>Public Free Speech {{ GameSessionStore.publicFreeSpeech }}</div>
     <div>Public Safety {{ GameSessionStore.publicSafety }}</div>
