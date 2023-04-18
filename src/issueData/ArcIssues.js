9faac97b40627c0ea8c0573a3589d004c713e5ad
Randy Lubin <randylubin@gmail.com>
2023-04-17 21:10:15 -0700

    more arc issues

--- src/issueData/ArcIssues.js
+++ src/issueData/ArcIssues.js
@@ -13,8 +13,17 @@ const ArcIssues = new IssueDatabase()
 
 let ArcLookup = {
   NUDITY: { earliestRound: 1 },
+  NUDE_ART: { earliestRound: 1 },
   HEALTHCARE: { earliestRound: 2 },
   ELECTION: { earliestRound: 3 },
+  POSTELECT: { earliestRound: 99 },
+  OFFPLAT1: { earliestRound: 1 },
+  OFFPLAT2: { earliestRound: 1 },
+  BRIGADE: { earliestRound: 1 },
+  COPYRIGHT: { earliestRound: 1 },
+  INTL: { earliestRound: 1 },
+  BETAAI: { earliestRound: 99 },
+  BETTERAI: { earliestRound: 99 },
 }
 
 axios
