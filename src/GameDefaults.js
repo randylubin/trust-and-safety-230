dced02de54f659fcd39da1010021b438d8cefdda
Randy Lubin <randylubin@gmail.com>
2023-04-21 13:36:32 -0700

    calibration

--- src/GameDefaults.js
+++ src/GameDefaults.js
@@ -1,10 +1,10 @@
 export const GameDefaults = {
   // ROUNDS
   // Round timing
-  roundLength: 100, // default seconds for a round
+  roundLength: 120, // default seconds for a round
   slowModeTick: 0.5, // time change per tick for slow mode (regular mode is 1 second per tick)
   extraTimeForLastCard: -5, // seconds after round clock hits 0
-  genericDrawLikelihood: 1, // odds of drawing a generic card each second
+  genericDrawLikelihood: 0.33, // odds of drawing a generic card each second
   // Round setup
   minimumStartingQueueLength: 5, // min number of cards at start of a round
   maxCarryoverLength: 5, // max number of cards at start of a round (arcs might push this over)
