cf75824ecb4900749eabd22fa61c4bc5c20a2998
Randy Lubin <randylubin@gmail.com>
2023-05-01 14:23:06 -0700

    reduce chunk size

--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,26 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        autoprefixer,// add options if needed
+        autoprefixer, // add options if needed
       ],
-    }
+    },
   },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          } else if (id.includes('src')) {
+            return 'app'
+          }
+        },
+      },
     },
   },
 })
